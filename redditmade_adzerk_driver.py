a19e500c2d97c651e7822a782753598e7cf9a476
Chris Stephens <cstephens@redditgifts.com>
2014-08-28 10:39:15 -0600

    now accepting multiple subreddits

--- redditmade_adzerk_driver.py
+++ redditmade_adzerk_driver.py
@@ -8,12 +8,12 @@ import json
 import traceback
 
 # test api key
-# adzerk.API_KEY = '1C2FC75BA96B6A44BFA96C1AD82545272FD6'
+adzerk.API_KEY = '1C2FC75BA96B6A44BFA96C1AD82545272FD6'
 redditmade_url = 'http://localhost:8000/api/v1/campaigns?linked=true&launched=True'
-# campaign_id = '140826'
-# advertiser_id = '70675'
-# priority_id = '61083'
-# publisher_id= '21194'
+campaign_id = '140826'
+advertiser_id = '70675'
+priority_id = '61083'
+publisher_id= '21194'
 
 ad_type_id = {
     'rectangle': 5,
@@ -21,47 +21,68 @@ ad_type_id = {
 }
 
 success = []
+warn = []
 errors = []
-whitelist = ['madclownlove', 'chrisrox']
+whitelist = ['madclownlove', 'chrisrox', 'testes']
 
-# **** TODO - These are the keys/ids for reddit.com only uncomment for golive! ****
-adzerk.API_KEY = ''
-campaign_id = '140812'
-advertiser_id = '44176'
-priority_id = '61020'
-publisher_id = '7589'
+# **** TODO_cstephens - These are the keys/ids for reddit.com only uncomment for golive! ****
+# adzerk.API_KEY = ''
+# campaign_id = '140812'
+# advertiser_id = '44176'
+# priority_id = '61020'
+# publisher_id = '7589'
 
 
-def download_campaign_image(campaign_slug, img_url):
-    path = 'campaign_art/' + campaign_slug
+def download_campaign_image(image_path, img_url):
     resp = requests.get(img_url, stream=True)
     if resp.status_code > 400:
         raise Exception('Unable to get tshit image, cannot build ad.')
 
-    with open(path, 'wb') as f:
+    with open(image_path, 'wb') as f:
         for chunk in resp.iter_content():
             f.write(chunk)
-    return path
+    return image_path
 
 
 def create_campaign(campaign):
+    if not campaign['goal']:
+        warn.append("%s doesn't have a goal set." % campaign['slug'])
+        return
+
+    keywords = [campaign['subreddit']]
+    if campaign['alt_subreddits']:
+        keywords = keywords + campaign['alt_subreddits'].split(',')
+    # TODO_cstephens - stops us from linking live subreddits. remove when we go live.
+    if [x for x in keywords if x not in whitelist]:
+        warn.append("%s is linked to non-test subreddits. Ignoring." % campaign['slug'])
+        return
+
     goal = (float(campaign['goal']) * float(campaign['price_in_cents'])) / 100
     funded = float(campaign['funded'])
     metadata = '{"percent":"%f"}' % funded
     ad_text = "%d%% funded of $%d goal" % (funded*100, goal)
+    custom_ads = campaign['adzerk_three_by_one_image'] or campaign['adzerk_medium_rectangle_image']
+    shirt_image = None 
+    keyword_string = '\n'.join(keywords)
 
-    if campaign['subreddit'] not in whitelist:
-        raise Exception("Non Test subreddit!!! Not gonna make it")
-
-    shirt_image = download_campaign_image(campaign['slug'], campaign['img'])
+    if campaign['product_type'] == 'shirt' and not custom_ads:
+        shirt_image = download_campaign_image('campaign_art/' + campaign['slug'], campaign['img'])
+    
+    elif custom_ads:
+        # If custom campaign, just download art directly and treat as compiled templates for now
+        download_campaign_image('compiled_templates/' + campaign['slug'] + '_3x1.png',
+                                campaign['adzerk_three_by_one_image'])
+        download_campaign_image('compiled_templates/' + campaign['slug'] + '_med_rect.png',
+                                campaign['adzerk_medium_rectangle_image'])
 
     # Make the flight in the adzerk api
     new_ad = adzerk.Flight.create(CampaignId=campaign_id, Name=campaign['slug'], StartDate=time.strftime("%m/%d/%Y"),
-                                  Price=0.0, Impressions=1, IsUnlimited=True, PriorityId=priority_id, IsDeleted=False,
-                                  IsActive=True, Keywords=campaign['subreddit'], NoEndDate=True, GoalType=2)
+                                  Price=0.0, Impressions=100, IsUnlimited=True, PriorityId=priority_id, IsDeleted=False,
+                                  IsActive=True, Keywords=keyword_string, NoEndDate=True, GoalType=2)
 
     # Build the 300 x 100 size ad
-    image_builder.build_3x1_ad(image_name=campaign['slug']+'_3x1.png', shirt_image=shirt_image)
+    if shirt_image:
+        image_builder.build_3x1_ad(image_name=campaign['slug']+'_3x1.png', shirt_image=shirt_image)
     ad_3x1_path = image_builder.update_progress(image_name=campaign['slug']+'_3x1.png', text_offset=(11, 55),
                                                 text=ad_text, bar_offset=(10, 76), bar_size=(155, 14), percent=funded,
                                                 goal=goal)
@@ -76,7 +97,9 @@ def create_campaign(campaign):
                                     Impressions=1, DistributionType=1)
 
     # Build the 300 x 250 size ad
-    image_builder.build_rectangle_ad(campaign['slug']+'_med_rect.png', campaign['subreddit'], shirt_image=shirt_image)
+    if shirt_image:
+        image_builder.build_rectangle_ad(campaign['slug']+'_med_rect.png', campaign['subreddit'],
+                                         shirt_image=shirt_image)
     ad_rect_path = image_builder.update_progress(image_name=campaign['slug']+'_med_rect.png', text_offset=(55, 204),
                                                  text=ad_text, bar_offset=(12, 222), bar_size=(272, 14), percent=funded,
                                                  goal=goal)
@@ -89,16 +112,35 @@ def create_campaign(campaign):
                                     PublisherAccountId=publisher_id, IsDeleted=False, Percentage=100,
                                     Creative={'Id': creative_rect.Id}, IsActive=True, FlightId=new_ad.Id,
                                     Impressions=1, DistributionType=1)
+    success.append("creating [%s]" % campaign['slug'])
     return True
+
+#Will eventually lose flight_id and have to get the ad by campaign_slug
+def update_keywords(campaign_slug, keywords, flight_id):
+    # TODO_cstephens - stops us from linking live subreddits. remove when we go live.
+    if [x for x in keywords if x not in whitelist]:
+        warn.append("%s is linked to non-test subreddits. Not updating flight." % campaign['slug'])
+        return
+    ad = adzerk.Flight.get(flight_id)
+    ad.Keywords = '\n'.join(keywords)
+    ad.save()
+    print "flight updated"
 
 
 def update_campaign(campaign, ad):
     print "updating " + campaign['slug']
     goal = (float(campaign['goal']) * float(campaign['price_in_cents'])) / 100
     percent = float(campaign['funded'])
-    percent = .45  # TODO remove when going live
+    percent = .45  # TODO_cstephens - remove when going live
     metadata = '{"percent":"%f"}' % percent
     ad_text = "%d%% funded of $%d goal" % (percent*100, goal)
+    
+    # subreddit targeting
+    keywords = [campaign['subreddit']]
+    if campaign['alt_subreddits']:
+        keywords = keywords + campaign['alt_subreddits'].split(',')
+    if '\n'.join(keywords) != ad.Keywords:
+        update_keywords(campaign['slug'], keywords, ad.Id)
 
     for creative_map in ad.CreativeMaps:
         if creative_map.Creative.AdTypeId == ad_type_id['3x1']:
@@ -163,7 +205,7 @@ for ad in adzerk_ads:
 
 # create/update/delete based on the state of the two objects
 for slug, campaign in reddit_data.iteritems():
-    # if campaign[''] # isLaunched TODO!!
+    # if campaign[''] # isLaunched TODO_cstephens!!
     # if RM=true
     if slug in adzerk_data:
         try:
@@ -179,7 +221,6 @@ for slug, campaign in reddit_data.iteritems():
     else:
         try:
             create_campaign(campaign)
-            success.append("creating [%s]" % campaign['slug'])
         except:
             print "Unable to create campaign for %s" % campaign['slug']
             traceback.print_exc()
@@ -207,6 +248,7 @@ print "----------------results------------------"
 print "-----------------------------------------"
 print "Successes: "
 print success
-
+print "Warns: "
+print warn
 print "Errors: "
 print errors
